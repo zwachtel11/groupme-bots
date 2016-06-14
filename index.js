bf5931d502c05749259da77422dc6be6515dee80
zwachtel11 <zwachtel11@gmail.com>
2016-06-14 18:38:47 -0400

    testhggfredgf

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ http        = require('http');
 director    = require('director');
 cool        = require('cool-ascii-faces');
 bot         = require('./bot.js');
+request = require('request');
 
 router = new director.http.Router({
   '/' : {
