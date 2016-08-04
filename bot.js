8d63e193f1377804cddf98a925463c019b79468e
zwachtel11 <zwachtel11@gmail.com>
2016-08-03 23:44:12 -0400

    token library

--- bot.js
+++ bot.js
@@ -148,32 +148,6 @@ function postMessage1() {
       }
   });
 
-  function postMessage2(message) {
-  var botResponse, options, body, botReq;
-
-  botResponse = message;
-
-  options = {
-    hostname: 'api.groupme.com',
-    path: '/v3/bots/post',
-    method: 'POST'
-  };
-
-  body = {
-    "bot_id" : botID,
-    "text" : botResponse
-  };
-
-  console.log('sending ' + botResponse + ' to ' + botID);
-
-  botReq = HTTPS.request(options, function(res) {
-      if(res.statusCode == 202) {
-        console.log('202 response');
-      } else {
-        console.log('rejecting bad status code from groupme:' + res.statusCode);
-      }
-  });
-
   botReq.on('error', function(err) {
     console.log('error posting message '  + JSON.stringify(err));
   });
@@ -184,6 +158,41 @@ function postMessage1() {
 
     }
 });
+
+}
+function postMessage2(message) {
+var botResponse, options, body, botReq;
+
+botResponse = message;
+
+options = {
+  hostname: 'api.groupme.com',
+  path: '/v3/bots/post',
+  method: 'POST'
+};
+
+body = {
+  "bot_id" : botID,
+  "text" : botResponse
+};
+
+console.log('sending ' + botResponse + ' to ' + botID);
+
+botReq = HTTPS.request(options, function(res) {
+    if(res.statusCode == 202) {
+      console.log('202 response');
+    } else {
+      console.log('rejecting bad status code from groupme:' + res.statusCode);
+    }
+});
+
+botReq.on('error', function(err) {
+  console.log('error posting message '  + JSON.stringify(err));
+});
+botReq.on('timeout', function(err) {
+  console.log('timeout posting message '  + JSON.stringify(err));
+});
+botReq.end(JSON.stringify(body));
 
 }
 
